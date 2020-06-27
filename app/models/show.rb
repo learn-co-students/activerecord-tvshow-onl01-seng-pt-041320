cd805cffde5f3bc98d73828afa0928dbe561a66d
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-06-26 17:22:52 -0700

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,12 @@ class Show < ActiveRecord::Base
     def Show::most_popular_show
         Show.select("id, name, network, day, MAX(rating), seasons, season").first
     end
+
+    def Show::lowest_rating
+        Show.minimum("rating")
+    end
+
+    def Show::least_popular_show
+        Show.select("id, name, network, day, MIN(rating), seasons, season").first
+    end
 end
