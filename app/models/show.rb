e422ea4c82811d2b02645c2e9bc7eecce78c403a
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-06-26 17:19:21 -0700

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,4 +1,9 @@
 class Show < ActiveRecord::Base
-    def highest_rating
+    def Show::highest_rating
+        Show.maximum("rating")
+    end
+
+    def Show::most_popular_show
+        Show.select("id, name, network, day, MAX(rating), seasons, season").first
     end
 end
